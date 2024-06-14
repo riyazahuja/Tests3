8c545b4691a61cdd218f5a0e1779f64383866356
riyazahuja <riyaza@andrew.cmu.edu>
2024-06-14 11:21:38 -0400

    more tests

--- Tests3/Basic.lean
+++ Tests3/Basic.lean
@@ -1,16 +1,57 @@
 import Mathlib.Tactic
 
-variable (P Q R S : Prop)
+theorem modus_tollens (p q : Prop) : (p → q) → (¬q → ¬p) := by
+  intro pq nq
+  by_contra np
+  have q' : q := pq np
+  contradiction
 
-example (h : ¬ (P ∨ Q)) : ¬ P ∧ ¬ Q := by
+theorem and_associative (p q r : Prop) : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := by
   constructor
-  intro p
-  have duh : P∨ Q := by
-    left
-    exact p
-  exact h duh
-  intro q
-  have duh : P∨ Q := by
-    right
-    exact q
-  exact h duh
+  . intro h
+    rcases h with ⟨hpq,hr⟩
+    rcases hpq with ⟨hp,hq⟩
+    constructor
+    . exact hp
+    . constructor
+      . exact hq
+      . exact hr
+  . intro h
+    rcases h with ⟨hp,hqr⟩
+    rcases hqr with ⟨hq,hr⟩
+    constructor
+    . constructor
+      . exact hp
+      . exact hq
+    . exact hr
+
+theorem distributivity_or_and (p q r : Prop) : (p ∨ q) ∧ (p ∨ r) ↔ p ∨ (q ∧ r) := by
+  constructor
+  . intro h
+    rcases h with ⟨hpq,hpr⟩
+    rcases hpq with hp|hq
+    . rcases hpr with _|_
+      . left
+        exact hp
+      . left
+        exact hp
+    . rcases hpr with hp|hr
+      . left
+        exact hp
+      . right
+        constructor
+        . exact hq
+        . exact hr
+  . intro h
+    rcases h with hp|hqr
+    . constructor
+      . left
+        exact hp
+      . left
+        exact hp
+    . rcases hqr with ⟨hq,hr⟩
+      constructor
+      . right
+        exact hq
+      . right
+        exact hr
